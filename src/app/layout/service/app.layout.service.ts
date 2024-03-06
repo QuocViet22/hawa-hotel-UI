0bb0f8d6c2db06057858377c7b5b3fcc9c1b4731
QuocViet <viethoangquoc201@gmail.com>
2024-03-06 22:28:48 +0700

    set up infrastructure

---
+++ src/app/layout/service/app.layout.service.ts
@@ -0,0 +1,74 @@
+import { Injectable, effect, signal } from '@angular/core';
+import { Subject } from 'rxjs';
+
+export interface AppConfig {
+  inputStyle: string;
+  colorScheme: string;
+  theme: string;
+  ripple: boolean;
+  menuMode: string;
+  scale: number;
+}
+
+interface LayoutState {
+  staticMenuDesktopInactive: boolean;
+  overlayMenuActive: boolean;
+  profileSidebarVisible: boolean;
+  configSidebarVisible: boolean;
+  staticMenuMobileActive: boolean;
+  menuHoverActive: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LayoutService {
+  _config: AppConfig = {
+    ripple: false,
+    inputStyle: 'outlined',
+    menuMode: 'static',
+    colorScheme: 'light',
+    theme: 'lara-light-indigo',
+    scale: 14,
+  };
+
+  config = signal<AppConfig>(this._config);
+
+  state: LayoutState = {
+    staticMenuDesktopInactive: false,
+    overlayMenuActive: false,
+    profileSidebarVisible: false,
+    configSidebarVisible: false,
+    staticMenuMobileActive: false,
+    menuHoverActive: false,
+  };
+
+  private overlayOpen = new Subject<any>();
+
+  overlayOpen$ = this.overlayOpen.asObservable();
+
+  constructor() {
+    effect(() => {
+      const config = this.config();
+    });
+  }
+
+  onMenuToggle() {
+    if (this.isDesktop()) {
+      this.state.staticMenuDesktopInactive = !this.state.staticMenuDesktopInactive;
+    } else {
+      this.state.staticMenuMobileActive = !this.state.staticMenuMobileActive;
+      if (this.state.staticMenuMobileActive) {
+        this.overlayOpen.next(null);
+      }
+    }
+  }
+
+  isDesktop() {
+    return window.innerWidth > 991;
+  }
+
+  isMobile() {
+    return !this.isDesktop();
+  }
+}
